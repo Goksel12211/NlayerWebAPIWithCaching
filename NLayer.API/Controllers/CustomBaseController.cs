40260f2f63a2fa808c760d47438f92d8609604e5
goksel12211 <carryvayne12211@gmail.com>
2023-08-28 02:03:51 +0300

    Add Product Controller

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction] // Bunu yazmazsak swagger bunu endpoint olarak tanımlar.
+        public IActionResult CreateActionResult<T>(CustomResponseDTO<T> response)
+        {
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode,
+                };
+            }
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
---
+++ NLayer.Core/DTOs/NoContentDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{ // Boş data için kullanıldı.
+    public class NoContentDTO
+    {
+    }
+}
