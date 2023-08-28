d3aaaba5ec9d030c06ae7e9b5f3c9900a23455bf
goksel12211 <carryvayne12211@gmail.com>
2023-08-28 20:59:58 +0300

    Add products via categoryId

--- NLayer.Service/Mapping/MapProfile.cs
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -19,6 +19,7 @@ namespace NLayer.Service.Mapping
             CreateMap<ProductFeature, ProductFeatureDTO>().ReverseMap();
             CreateMap<ProductUpdateDTO, Product>();
             CreateMap<Product, ProductWithCategoryDTO>();
+            CreateMap<Category, CategoryWithProductsDTO>();
         }
     }
 }
